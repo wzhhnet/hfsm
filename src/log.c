98287f165659d8414c7e597b1ae2f02b500540c2
wanch <wanchaohui@mxnavi.com>
2022-12-20 10:05:52 +0800

    fix title comments

--- src/log.c
+++ src/log.c
@@ -1,12 +1,23 @@
-/********** Copyright(C) 2021 MXNavi Co.,Ltd. ALL RIGHTS RESERVED **********/
-/****************************************************************************
-*   FILE NAME   : log.c
-*   CREATE DATE : 2021-12-27
-*   MODULE      : Hierarchy tree
-*   AUTHOR      : wanch
-*---------------------------------------------------------------------------*
-*   MEMO        :
-*****************************************************************************/
+/*
+ * Loger for HFSM
+ *
+ * Author wanch
+ * Date 2021/12/27
+ * Email wzhhnet@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <log.h>
 #include <stdarg.h>
 #include <stdio.h>
